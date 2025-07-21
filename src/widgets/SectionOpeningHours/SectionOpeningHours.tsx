e4d7e00c6f409e51f2373b34c1126762e7b5aa2d
MEDOYED <mmax26072005@gmail.com>
2025-07-21 15:12:17 +0200

    feat(SectionOpeningHours): translate from ukrainian to polish

--- src/widgets/SectionOpeningHours/SectionOpeningHours.tsx
+++ src/widgets/SectionOpeningHours/SectionOpeningHours.tsx
@@ -1,43 +1,49 @@
+import dataProject from "../../shared/data/dataProject";
+
 import s from "./SectionOpeningHours.module.scss";
 
 const SectionOpeningHours = () => {
   return (
     <section className={s.section}>
-      <h2 className={s.title}>— ГОДИНИ РОБОТИ —</h2>
+      <h2 className={s.title}>— GODZINY PRACY —</h2>
       <ul className={s.list}>
         <li className={s.list__item}>
-          <span>Понеділок</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Poniedziałek</span>
+          <span className={s.hours}>{dataProject.openHours.monday}</span>
         </li>
 
         <li className={s.list__item}>
-          <span>Вівторок</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Wtorek</span>
+          <span className={s.hours}>{dataProject.openHours.tuesday}</span>
         </li>
 
         <li className={s.list__item}>
-          <span>Середа</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Środa</span>
+          <span className={s.hours}>{dataProject.openHours.wednesday}</span>
         </li>
+
         <li className={s.list__item}>
-          <span>Четвер</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Czwartek</span>
+          <span className={s.hours}>{dataProject.openHours.thursday}</span>
         </li>
+
         <li className={s.list__item}>
-          <span>П'ятниця</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Piątek</span>
+          <span className={s.hours}>{dataProject.openHours.friday}</span>
         </li>
+
         <li className={s.list__item}>
-          <span>Субота</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Sobota</span>
+          <span className={s.hours}>{dataProject.openHours.saturday}</span>
         </li>
+
         <li className={s.list__item}>
-          <span>Неділя</span>
-          <span className={s.hours}>12h — 15h</span>
+          <span>Niedziela</span>
+          <span className={s.hours}>{dataProject.openHours.sunday}</span>
         </li>
       </ul>
       <p className={s.subtitle}>
-        Запрошуємо до нашого закладу у відкриті години.
+        Zapraszamy do naszej restauracji w godzinach otwarcia
       </p>
     </section>
   );
