1b0363f5c64619a5e65b808a3bd5f6a92339518b
Roverp <yar.zubaha@proton.me>
2025-07-22 01:05:10 +0200

    chore(SectionHero): plug in ButtonForm

--- src/widgets/sections/SectionHero/SectionHero.tsx
+++ src/widgets/sections/SectionHero/SectionHero.tsx
@@ -1,3 +1,5 @@
+import { ButtonForm } from "@shared";
+
 import dataProject from "../../../shared/data/dataProject";
 
 import cn from "./SectionHero.module.scss";
@@ -6,6 +8,7 @@ const SectionHero = () => {
   return (
     <section className={cn.heroSection}>
       <span className={cn.phoneNumbers}>{dataProject.phoneNumbers}</span>
+      <ButtonForm />
       <h1 className={cn.companyName}>{dataProject.companyName}</h1>
     </section>
   );
