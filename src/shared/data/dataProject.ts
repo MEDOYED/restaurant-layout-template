6665242046ae9616a494da317023d9effec1c158
MEDOYED <mmax26072005@gmail.com>
2025-07-22 19:14:34 +0200

    feat(dataProject.ts): filled in data for Aiart Restourant

--- src/shared/data/dataProject.ts
+++ src/shared/data/dataProject.ts
@@ -14,36 +14,36 @@ type Day = {
 
 const dataProject: DataProject = {
   companyName: "Asiart Restaurant",
-  phoneNumbers: "11 2564-7445 | 11 2534-7106",
+  phoneNumbers: "690 628 540",
 
   openHours: [
     {
       day: "Poniedziałek",
-      time: "09:00–23:00",
+      time: "nie pracujemy",
     },
     {
       day: "Wtorek",
-      time: "09:00–23:00",
+      time: "11:00–22:00",
     },
     {
       day: "Środa",
-      time: "09:00–23:00",
+      time: "11:00–22:00",
     },
     {
       day: "Czwartek",
-      time: "09:00–00:00",
+      time: "11:00–22:00",
     },
     {
       day: "Piątek",
-      time: "09:00–02:00",
+      time: "12:00–22:00",
     },
     {
       day: "Sobota",
-      time: "09:00–02:00",
+      time: "12:00–22:00",
     },
     {
       day: "Niedziela",
-      time: "09:00–23:00",
+      time: "12:00–22:00",
     },
   ],
 
